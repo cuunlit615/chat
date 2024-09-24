c67c3c0a45756f37e744283505c3adb92b77be01
Ted Yu <o23958078@gmail.com>
2024-09-24 23:24:12 +0800

    add test.py

---
+++ test.py
@@ -0,0 +1,29 @@
+def readfile(filename):
+    lines = []
+    with open(filename, 'r', encoding='utf-8-sig') as f :
+        for line in f:
+            lines.append(line.strip())
+    return lines
+def convert(lines):
+    new = []
+    person = None
+    for line in lines:
+        if line == 'Allen':
+            person = 'Allen'
+            continue
+        elif line == 'Tom':
+            person = 'Tom'
+            continue
+        if person:
+            new.append(person + ':' + line)
+    return new
+def write(filename, lines):
+    with open(filename, 'w',  encoding='utf-8-sig') as f:
+        for line in lines:
+            f.write(line + '\n')
+def main():
+    lines = readfile('input.txt')
+    lines = convert(lines)
+    write('output.txt', lines)
+
+main()
