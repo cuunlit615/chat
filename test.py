55e6f24fbbdfc26acb307e7a1ad6abc8d0c1e2ef
cuunlit615 <114757891+cuunlit615@users.noreply.github.com>
2024-10-01 00:38:28 +0800

    Update test.py
    add comment

--- test.py
+++ test.py
@@ -1,29 +1,29 @@
-def readfile(filename):
+def readfile(filename):#讀檔
     lines = []
     with open(filename, 'r', encoding='utf-8-sig') as f :
         for line in f:
-            lines.append(line.strip())
+            lines.append(line.strip())#將檔案內容竹行加至lines列表
     return lines
-def convert(lines):
+def convert(lines):#句子轉換
     new = []
     person = None
     for line in lines:
-        if line == 'Allen':
+        if line == 'Allen':#如果該行為Allen，將 Allen 存入 person
             person = 'Allen'
             continue
-        elif line == 'Tom':
+        elif line == 'Tom':#如果該行為Tom，將 Tom 存入 person
             person = 'Tom'
             continue
-        if person:
-            new.append(person + ':' + line)
+        if person:#如果person == True
+            new.append(person + ':' + line)#new列表添加person:line
     return new
-def write(filename, lines):
+def write(filename, lines):#寫入
     with open(filename, 'w',  encoding='utf-8-sig') as f:
         for line in lines:
             f.write(line + '\n')
 def main():
-    lines = readfile('input.txt')
-    lines = convert(lines)
-    write('output.txt', lines)
+    lines = readfile('input.txt')#讀檔+定義lines
+    lines = convert(lines)#lines轉換
+    write('output.txt', lines)#寫入
 
 main()
